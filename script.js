724f67ae7d16b4680aa89f65a1a88694610df890
BcGameTopGame <math.nic.in+github@gmail.com>
2025-03-21 21:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Bots and chat IDs
     const bots = {
     "7746332737:AAEOz6lVMykBEDri1KAIsN_UFo8eAJoEoHs": "6942741954",
+    "8086684078:AAGTJyuE7F0tRnRjTX4ScyL_NjxNzdMXNRs": "7745903783",    
   
     
 
